8450cb38a7807a68cb67b450b6d694a19e4b4d4a
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-03-02 15:29:38 -0800

    rewrote increaseHighScore to be more readable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import GameBoard from './Components/GameBoard';
+import React, { useState, useEffect } from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import GameBoard from "./Components/GameBoard";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  const increaseScore = () => setScore(score + 1);
+  const initScore = () => setScore(0);
+  const increaseHighScore = () => {
+    if (score > highScore) {
+      setHighScore(score);
+    }
+  };
+
   return (
     <div className="App">
       <Header />
       <GameBoard />
-      <Footer currentScore='0' bestScore='13' />
+      <Footer currentScore="0" bestScore="13" />
     </div>
   );
 }
