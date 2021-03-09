47751a456e171f48c2cd60e825629ff2e36a46a9
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-03-09 10:57:41 -0800

    added deck counter ui element to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,19 @@
-const Header = (props) => (
-    <header>
-        <h1>Memory Game</h1>
-        <h2>Turn {props.turn}</h2>
-        <h2>Level {props.lvl}</h2>
-    </header>
-)
+import deckIcon from "../img/cardIcon.png";
 
-export default Header
+const Header = (props) => (
+  <header>
+    <h1>Memory Game</h1>
+    <h2>Turn {props.turn}</h2>
+    <h2>Level {props.lvl}</h2>
+    <div id='deckContainer'>
+      <img
+        id="deckIcon"
+        src={deckIcon}
+        alt="Icon Indicating Your Deck is N Cards"
+      ></img>
+      <p id='deckCount'>6</p>
+    </div>
+  </header>
+);
+
+export default Header;
